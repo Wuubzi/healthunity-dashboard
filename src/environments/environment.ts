795630ef7fdc7535736ede49d6e895cbfd15000e
wuubzi <carlosasalas321@gmail.com>
2025-12-15 02:17:55 -0500

    fixing problems

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     domain: 'dev-kv01j6hec10g7jva.us.auth0.com',
     clientId: 'XL1I9tTosUijoQn2nhzGZ6KIG4yRbGQE',
     audiencie: 'https://api.healthunity.com',
-    apiUrl: 'http://localhost:8080/api/v1',
+    apiUrl: 'https://healthunity.online/api/v1',
   },
 };
